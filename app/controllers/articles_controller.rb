f031d587de7cc0d8237f01991ceb41ef444307e1
Nick Inman <nick@theoatmeal.com>
2019-02-06 01:09:21 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
-
-  # add edit and update methods here
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
 end
